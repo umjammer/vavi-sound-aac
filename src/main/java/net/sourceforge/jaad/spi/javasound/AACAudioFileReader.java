cdd7b1b0228a877aa6151996775f5f229d837a16
Naohide Sano <namachapanda2005@gmail.com>
2022-03-04 21:52:20 +0900

    fix #2 implement format conversion provider

--- src/main/java/net/sourceforge/jaad/spi/javasound/AACAudioFileReader.java
+++ src/main/java/net/sourceforge/jaad/spi/javasound/AACAudioFileReader.java
@@ -1,7 +1,5 @@
 package net.sourceforge.jaad.spi.javasound;
 
-import net.sourceforge.jaad.adts.ADTSDemultiplexer;
-import net.sourceforge.jaad.aac.syntax.BitStream;
 import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
@@ -9,6 +7,10 @@ import java.io.FilterInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.List;
+import java.util.logging.Logger;
+
 import javax.sound.sampled.AudioFileFormat;
 import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioInputStream;
@@ -16,11 +18,22 @@ import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.UnsupportedAudioFileException;
 import javax.sound.sampled.spi.AudioFileReader;
 
+import net.sourceforge.jaad.aac.AACException;
+import net.sourceforge.jaad.aac.Decoder;
+import net.sourceforge.jaad.aac.syntax.BitStream;
+import net.sourceforge.jaad.adts.ADTSDemultiplexer;
+import net.sourceforge.jaad.mp4.MP4Container;
+import net.sourceforge.jaad.mp4.api.AudioTrack;
+import net.sourceforge.jaad.mp4.api.Movie;
+import net.sourceforge.jaad.mp4.api.Track;
+
 public class AACAudioFileReader extends AudioFileReader {
 
-	public static final AudioFileFormat.Type AAC = new AudioFileFormat.Type("AAC", "aac");
-	public static final AudioFileFormat.Type MP4 = new AudioFileFormat.Type("MP4", "mp4");
-	private static final AudioFormat.Encoding AAC_ENCODING = new AudioFormat.Encoding("AAC");
+    private static Logger logger = Logger.getLogger(AACAudioFileReader.class.getName());
+
+    public static final AudioFileFormat.Type AAC = new AudioFileFormat.Type("AAC", "aac");
+    public static final AudioFileFormat.Type MP4 = new AudioFileFormat.Type("MP4", "mp4");
+    public static final AudioFormat.Encoding AAC_ENCODING = new AudioFormat.Encoding("AAC");
 
     private static class LimitedInputStream extends FilterInputStream {
         static final String ERROR_MESSAGE_REACHED_TO_LIMIT = "stop reading, prevent form eof";
@@ -95,55 +108,73 @@ logger.fine("reached to limit");
 	}
 
 	private AudioFileFormat getAudioFileFormat(InputStream in, int mediaLength) throws UnsupportedAudioFileException, IOException {
-		final byte[] head = new byte[12];
-		in.read(head);
-		boolean canHandle = false;
-		if(new String(head, 4, 4).equals("ftyp"))
-			canHandle = true;
-		//This code is pulled directly from MP3-SPI.
-		else if ((head[0] == 'R') && (head[1] == 'I') && (head[2] == 'F') && (head[3] == 'F') && (head[8] == 'W') && (head[9] == 'A') && (head[10] == 'V') && (head[11] == 'E'))
-		{
-			canHandle = false;	//RIFF/WAV stream found
-		}
-		else if ((head[0] == '.') && (head[1] == 's') && (head[2] == 'n') && (head[3] == 'd'))
-		{
-			canHandle = false;	//AU stream found
-		}
-		else if ((head[0] == 'F') && (head[1] == 'O') && (head[2] == 'R') && (head[3] == 'M') && (head[8] == 'A') && (head[9] == 'I') && (head[10] == 'F') && (head[11] == 'F'))
-		{
-			canHandle = false;	//AIFF stream found
-		}
-		else if (((head[0] == 'M') | (head[0] == 'm')) && ((head[1] == 'A') | (head[1] == 'a')) && ((head[2] == 'C') | (head[2] == 'c')))
-		{
-			canHandle = false;	//APE stream found
-		}
-		else if (((head[0] == 'F') | (head[0] == 'f')) && ((head[1] == 'L') | (head[1] == 'l')) && ((head[2] == 'A') | (head[2] == 'a')) && ((head[3] == 'C') | (head[3] == 'c')))
-		{
-			canHandle = false;	//FLAC stream found
-		}
-		else if (((head[0] == 'I') | (head[0] == 'i')) && ((head[1] == 'C') | (head[1] == 'c')) && ((head[2] == 'Y') | (head[2] == 'y')))
-		{
-			canHandle = false;	//Shoutcast / ICE stream ?
-		}
-		else if (((head[0] == 'O') | (head[0] == 'o')) && ((head[1] == 'G') | (head[1] == 'g')) && ((head[2] == 'G') | (head[2] == 'g')))
-		{
-			canHandle = false;	//Ogg stream ?
-		}
-		else {
-			final BitStream bit = new BitStream(head);
-			try {
+        try {
+    		final byte[] head = new byte[12];
+    		in.read(head);
+    		boolean canHandle = false;
+    		AudioFileFormat.Type type = AAC;
+    		if (new String(head, 4, 4).equals("ftyp")) {
+    		    in.reset();
+    		    in.mark(1000);
+
+    		    // in position should be zero
+    	        MP4Container cont = new MP4Container(in);
+    	        Movie movie = cont.getMovie();
+    	        List<Track> tracks = movie.getTracks(AudioTrack.AudioCodec.AAC);
+    	        if (tracks.isEmpty()) throw new UnsupportedAudioFileException("movie does not contain any AAC track");
+    	        Track track = (AudioTrack) tracks.get(0);
+	            new Decoder(track.getDecoderSpecificInfo());
+
+    	        canHandle = true;
+    			type = MP4;
+    		//This code is pulled directly from MP3-SPI.
+    		} else if ((head[0] == 'R') && (head[1] == 'I') && (head[2] == 'F') && (head[3] == 'F') && (head[8] == 'W') && (head[9] == 'A') && (head[10] == 'V') && (head[11] == 'E'))
+    		{
+    			canHandle = false;	//RIFF/WAV stream found
+    		}
+    		else if ((head[0] == '.') && (head[1] == 's') && (head[2] == 'n') && (head[3] == 'd'))
+    		{
+    			canHandle = false;	//AU stream found
+    		}
+    		else if ((head[0] == 'F') && (head[1] == 'O') && (head[2] == 'R') && (head[3] == 'M') && (head[8] == 'A') && (head[9] == 'I') && (head[10] == 'F') && (head[11] == 'F'))
+    		{
+    			canHandle = false;	//AIFF stream found
+    		}
+    		else if (((head[0] == 'M') | (head[0] == 'm')) && ((head[1] == 'A') | (head[1] == 'a')) && ((head[2] == 'C') | (head[2] == 'c')))
+    		{
+    			canHandle = false;	//APE stream found
+    		}
+    		else if (((head[0] == 'F') | (head[0] == 'f')) && ((head[1] == 'L') | (head[1] == 'l')) && ((head[2] == 'A') | (head[2] == 'a')) && ((head[3] == 'C') | (head[3] == 'c')))
+    		{
+    			canHandle = false;	//FLAC stream found
+    		}
+    		else if (((head[0] == 'I') | (head[0] == 'i')) && ((head[1] == 'C') | (head[1] == 'c')) && ((head[2] == 'Y') | (head[2] == 'y')))
+    		{
+    			canHandle = false;	//Shoutcast / ICE stream ?
+    		}
+    		else if (((head[0] == 'O') | (head[0] == 'o')) && ((head[1] == 'G') | (head[1] == 'g')) && ((head[2] == 'G') | (head[2] == 'g')))
+    		{
+    			canHandle = false;	//Ogg stream ?
+    		}
+    		else {
 				ADTSDemultiplexer adts = new ADTSDemultiplexer(in);
-				canHandle = true;
-			}
-			catch(Exception e) {
-				canHandle = false;
-			}
-		}
-		if(canHandle) {
-			final AudioFormat format = new AudioFormat(AAC_ENCODING, AudioSystem.NOT_SPECIFIED, AudioSystem.NOT_SPECIFIED, mediaLength, AudioSystem.NOT_SPECIFIED, AudioSystem.NOT_SPECIFIED, true);
-			return new AudioFileFormat(AAC, format, AudioSystem.NOT_SPECIFIED);
-		}
-		else throw new UnsupportedAudioFileException();
+                new Decoder(adts.getDecoderSpecificInfo());
+
+	            canHandle = true;
+    		}
+
+    		if(canHandle) {
+    		    AudioFileFormat.Type afft = type;
+    			AudioFormat format = new AudioFormat(AAC_ENCODING, AudioSystem.NOT_SPECIFIED, AudioSystem.NOT_SPECIFIED, AudioSystem.NOT_SPECIFIED, AudioSystem.NOT_SPECIFIED, AudioSystem.NOT_SPECIFIED, true, new HashMap<String, Object>() {{ put("type", afft); }});
+logger.fine("DEFINED: " + type);
+    			return new AudioFileFormat(type, format, mediaLength);
+    		} else {
+                throw new UnsupportedAudioFileException("no match sequence");
+    		}
+        } catch (Exception e) {
+logger.fine(e.getClass().getSimpleName() + ": " + e.getMessage());
+            throw (UnsupportedAudioFileException) new UnsupportedAudioFileException().initCause(e);
+        }
 	}
 
 	//================================================
@@ -153,15 +184,14 @@ logger.fine("reached to limit");
 		try {
 			if(!in.markSupported()) in = new BufferedInputStream(in);
 			synchronized (this) {
-			in.mark(1000);
+			    in.mark(1000);
 logger.finer("mark: " + in.available());
 			    needReset = true;
 			}
 			final AudioFileFormat aff = getAudioFileFormat(new LimitedInputStream(in), AudioSystem.NOT_SPECIFIED);
             synchronized (this) {
 logger.finer("before reset: " + in.available());
-			in.reset();
-			return new MP4AudioInputStream(in, aff.getFormat(), aff.getFrameLength());
+                in.reset();
 logger.finer("after reset: " + in.available());
                 needReset = false;
             }
@@ -172,6 +202,7 @@ logger.finer("mark2: " + in.available());
             }
             // in position should be zero
 logger.fine("format: " + aff.getFormat());
+            return new AudioInputStream(in, aff.getFormat(), aff.getFrameLength());
 		}
 		catch(UnsupportedAudioFileException e) {
 			throw e;
