f46d3abc7aff7d3c451ceed36a03f40cb2226e85
Naohide Sano <namachapanda2005@gmail.com>
2022-03-04 21:52:56 +0900

    fix about logging

--- src/main/java/net/sourceforge/jaad/mp4/MP4Container.java
+++ src/main/java/net/sourceforge/jaad/mp4/MP4Container.java
@@ -48,15 +48,15 @@ import net.sourceforge.jaad.mp4.boxes.impl.ProgressiveDownloadInformationBox;
 public class MP4Container {
 
 	static {
-		Logger log = Logger.getLogger("MP4 API");
-		for(Handler h : log.getHandlers()) {
-			log.removeHandler(h);
-		}
-		log.setLevel(Level.WARNING);
-
-		final ConsoleHandler h = new ConsoleHandler();
-		h.setLevel(Level.ALL);
-		log.addHandler(h);
+//		Logger log = Logger.getLogger("MP4 API");
+//		for(Handler h : log.getHandlers()) {
+//			log.removeHandler(h);
+//		}
+//		log.setLevel(Level.WARNING);
+//
+//		final ConsoleHandler h = new ConsoleHandler();
+//		h.setLevel(Level.ALL);
+//		log.addHandler(h);
 	}
 	private final MP4InputStream in;
 	private final List<Box> boxes;
